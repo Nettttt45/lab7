d6b6fc8197bbbfa08d12e56b7c124124804e28d9
Nettttt45 <netnadee@gmail.com>
2025-03-03 12:00:20 +0700

    เสร็จแล้ว

---
+++ Assets/Airplanel.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class Airplane : MonoBehaviour
+{
+    public float thrustPower = 500f;
+    public float liftBooster = 5f;
+    public float drag = 0.02f;
+    public float angularDrag = 0.01f;
+
+    [SerializeField] float yawPower = 50f; 
+    [SerializeField] float pitchPower = 50f; 
+    [SerializeField] float rollPower = 30f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+    }
+
+    void FixedUpdate()
+    {
+        ApplyThrust();
+        ApplyLift();
+        ApplyDrag();
+        HandleRotation();
+    }
+
+    void ApplyThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * thrustPower, ForceMode.Acceleration);
+        }
+    }
+
+    void ApplyLift()
+    {
+        Vector3 lift = Vector3.Project(rb.velocity, transform.forward);
+        rb.AddForce(-transform.up * lift.magnitude * liftBooster);
+
+    }
+
+    void ApplyDrag()
+    {
+        rb.linearDamping = rb.linearVelocity.magnitude * drag;
+        rb.angularDamping = rb.linearVelocity.magnitude * angularDrag;
+    }
+
+    void HandleRotation()
+    {
+        float yaw = Input.GetAxis("Horizontal") * yawPower;
+        float pitch = Input.GetAxis("Vertical") * pitchPower;
+        float roll = Input.GetAxis("Roll") * rollPower;
+
+        rb.AddTorque(transform.up * yaw);   
+        rb.AddTorque(transform.right * pitch); 
+        rb.AddTorque(transform.forward * roll); 
+    }
+}
